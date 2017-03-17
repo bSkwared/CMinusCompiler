36f32acd3a1986fbd46b3d26041e28a6bbc4734a
timo352 <timothyglensmith@cedarville.edu>
2017-03-17 16:48:31 -0400

    moved files to declaration folder

---
+++ Parser/src/parser/productions/declaration/FunDeclaration.java
@@ -0,0 +1,37 @@
+package parser.productions;
+
+/**
+ *
+ * @author Timothy Smith and Blake Lasky
+ */
+public class FunDeclaration extends Declaration {
+    
+    
+    private static final TokenType[] FIRST  = { TokenType.OPEN_PARAM };
+
+    private static final TokenType[] FOLLOW = { TokenType.VOID,
+                                                TokenType.INT,
+                                                TokenType.EIF };
+    
+    public static boolean inFirst(TokenType type) {
+        return inSet(FIRST_SET, type);
+    }
+
+    public static boolean inFollow(TokenType type) {
+        return inSet(FOLLOW_SET, type);
+    }
+
+    private static boolean inSet(TokenType[] set, TokenType tok) {
+        boolean result = false;
+
+        for (TokenType t : set) {
+            if (tok == t) {
+                result = true;
+                break;
+            }
+        }
+
+        return result;
+    }
+
+}
