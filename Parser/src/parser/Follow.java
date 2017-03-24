991b41cd251172389143f70211d00f2aaacf6b01
bSkwared <gh@truebearsfan.com>
2017-03-24 13:52:32 -0400

    Format Follow

--- Parser/src/parser/Follow.java
+++ Parser/src/parser/Follow.java
@@ -1,45 +1,43 @@
 /**
  * @author Blake Lasky and Timothy Smith
- * @version 1.0
- * File: Follow.java
- * Created: March 2017
+ * @version 1.0 File: Follow.java Created: March 2017
  *
  * Description: This file provides us with the follow sets for all relevant
- *				productions in our CMinusGrammar
+ * productions in our CMinusGrammar
  */
-
 package parser;
 
 import parser.scanner.Token.TokenType;
 
 public class Follow {
-    public static TokenType[] Expression = { TokenType.CLOSE_PAREN, 
-												TokenType.SEMICOLON,
-												TokenType.COMMA,
-												TokenType.CLOSE_BRACKET};
-	
-	public static TokenType[] SimpleExpressionPrime = { TokenType.CLOSE_PAREN, 
-															TokenType.SEMICOLON,
-															TokenType.COMMA,
-															TokenType.CLOSE_BRACKET};
-	
-	public static TokenType[] AdditiveExpression = { TokenType.CLOSE_PAREN, 
-															TokenType.SEMICOLON,
-															TokenType.COMMA,
-															TokenType.CLOSE_BRACKET};
-	
-	public static TokenType[] VarCall = { TokenType.CLOSE_PAREN, 
-											TokenType.SEMICOLON,
-											TokenType.COMMA,
-											TokenType.CLOSE_BRACKET,
-											TokenType.ADD,
-											TokenType.SUB,
-											TokenType.MULT,
-											TokenType.DIV,
-											TokenType.LTE,
-											TokenType.GTE,
-											TokenType.LT,
-											TokenType.GT,
-											TokenType.EQUAL,
-											TokenType.NOT_EQUAL};
+
+    public static TokenType[] Expression = {TokenType.CLOSE_PAREN,
+                                            TokenType.SEMICOLON,
+                                            TokenType.COMMA,
+                                            TokenType.CLOSE_BRACKET};
+
+    public static TokenType[] SimpleExpressionPrime = {TokenType.CLOSE_PAREN,
+                                                       TokenType.SEMICOLON,
+                                                       TokenType.COMMA,
+                                                       TokenType.CLOSE_BRACKET};
+
+    public static TokenType[] AdditiveExpression = {TokenType.CLOSE_PAREN,
+                                                    TokenType.SEMICOLON,
+                                                    TokenType.COMMA,
+                                                    TokenType.CLOSE_BRACKET};
+
+    public static TokenType[] VarCall = {TokenType.CLOSE_PAREN,
+                                         TokenType.SEMICOLON,
+                                         TokenType.COMMA,
+                                         TokenType.CLOSE_BRACKET,
+                                         TokenType.ADD,
+                                         TokenType.SUB,
+                                         TokenType.MULT,
+                                         TokenType.DIV,
+                                         TokenType.LTE,
+                                         TokenType.GTE,
+                                         TokenType.LT,
+                                         TokenType.GT,
+                                         TokenType.EQUAL,
+                                         TokenType.NOT_EQUAL};
 }
