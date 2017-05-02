9d72adfa3cc9888d3df76813b134023d7606de1b
Timothy <Timothy@163.11.177.228>
2017-05-02 15:40:17 -0400

    Added all error output file

--- Compiler/src/parser/productions/statement/ReturnStatement.java
+++ Compiler/src/parser/productions/statement/ReturnStatement.java
@@ -10,6 +10,11 @@
 
 package parser.productions.statement;
 
+import lowlevel.BasicBlock;
+import lowlevel.Function;
+import lowlevel.Operand;
+import lowlevel.Operation;
+import parser.CodeGenerationException;
 import parser.productions.expression.Expression;
 
 public class ReturnStatement extends Statement {
@@ -36,4 +41,31 @@ public class ReturnStatement extends Statement {
 		
 		return str;
     }
+	
+	@Override
+	// TODO: Add exit block
+	public void genCode(Function func) throws CodeGenerationException{
+		
+		BasicBlock currBlock = func.getCurrBlock();
+		
+		int regNum;
+		// genCode on Expression
+		if(returnExpression != null){
+			regNum = returnExpression.genCode(func);
+			
+			// move expression info to EAX
+			Operation op = new Operation(Operation.OperationType.ASSIGN, currBlock);
+			Operand eaxOper = new Operand(Operand.OperandType.MACRO, "EAX");
+			Operand expOper = new Operand(Operand.OperandType.REGISTER, regNum);
+			
+			op.setSrcOperand(0, expOper);
+			op.setDestOperand(0, eaxOper);
+			
+			currBlock.appendOper(op);
+		}
+		
+		// create ExitBlock if necessary		
+		// add JMP operation to ExitBlock
+		
+	}
 }
